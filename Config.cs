0cfd57df2d0d832ae257b8cee785cd4059469882
Kyryh <kyrykiller07@gmail.com>
2024-01-19 20:21:57 +0100

    Update en-US_Mods.UnlimitedGroundedItems.hjson

---
+++ Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader.Config;
+
+namespace UnlimitedGroundedItems {
+    internal class Config : ModConfig {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        [DefaultValue(400)]
+        [Range(2, 10000)]
+        [ReloadRequired]
+        public int groundedItemsCap;
+    }
+}
